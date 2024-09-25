83c9fc1e2ed6ef93a2468f3c94987b907fbd1e97
andersonsilvasw <andersonsilvaedif@hotmail.com>
2024-09-25 16:10:41 -0300

    Entrada de dados

---
+++ src/entradaDeDados.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class entradaDeDados {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Olá, qual o seu nome? ");
+        String nome = sc.next();
+        System.out.print("Olá " + nome + ", Qual a sua idade? ");
+        int idade = sc.nextInt();
+        System.out.print("Qual o seu sexo? (F para feminino ou M para masculino): ");
+        char sexo = sc.next().charAt(0);
+        System.out.print("E para finalizar, Qual o seu peso atual? ");
+        double peso = sc.nextDouble();
+
+        System.out.printf("Tudo bem %s, então só confirmando você tem %d anos, sexo: %s, " +
+                "e peso de " + peso + "KG, agora que temos seus dados iremos elaborar seu plano."
+                , nome, idade, sexo);
+    }
+}
