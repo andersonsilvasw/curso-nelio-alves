da872b10dce6544159b461b98e456241ddad3aea
andersonsilvasw <andersonsilvaedif@hotmail.com>
2024-10-01 18:49:00 -0300

    Exercicios da estrutura While

---
+++ src/exercicios/estruturaRepetitivaWhile/erw001.java
@@ -0,0 +1,18 @@
+package exercicios.estruturaRepetitivaWhile;
+
+import java.util.Scanner;
+
+public class erw001 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha Inválida!");
+            senha = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido!");
+        sc.close();
+    }
+}
